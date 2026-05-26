2480db69272274455ea4d5fb165d1947e9de0825
gabie762 <gabihenriques821@gmail.com>
2026-05-26 13:54:13 -0300

    adiciona entidade Oportunidade com novos atributos e cria repositório e serviço para gerenciamento de oportunidades

--- src/main/java/extensao/backend/entity/Oportunidade.java
+++ src/main/java/extensao/backend/entity/Oportunidade.java
@@ -1,4 +1,41 @@
 package extensao.backend.entity;
 
+import java.time.Instant;
+import java.util.List;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "oportunidades")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Oportunidade {
+
+    @Id
+    private String id;
+    private String projetoId;
+    private String titulo;
+    private String descricao;
+    private String sobreProjeto;
+    private List<String> objetivos;
+    private List<String> atividadesDesenvolvidas;
+    private List<String> comoParticipar;
+    private boolean certificado;
+    private List<String> requisitos;
+    private String prazoInscricao;
+    private String tipo; // ex: "bolsa", "voluntariado"
+    private String local;
+    private String cargaHoraria;
+    private String status;
+
+    @CreatedDate
+    private Instant criadoEm;
+
+    @LastModifiedDate
+    private Instant atualizadoEm;
 }
